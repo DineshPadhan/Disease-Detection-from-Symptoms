41428a285f2811bd507482d664b4a625da2bfbfc
Dinesh <74955280+DineshPadhan@users.noreply.github.com>
2025-02-25 15:13:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
-from sklearn.model_selection import train_test_split        #for splting and creating new dataset as a variable
+from sklearn.model_selection import train_test_split  # for splitting and creating new dataset as a variable
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import numpy as np
 import pickle
-
 
 
 # ===============================================================================================
@@ -46,39 +45,80 @@ app = Flask(__name__)
 # costume and helping function
 # ===============================================================================================
 def helper(dis):
-  descr = description[description['Disease'] == dis]['Description']
-  descr = "".join([w for w in descr])
+    descr = description[description['Disease'] == dis]['Description']
+    descr = "".join([w for w in descr])
 
-  pre = precaucation[precaucation['Disease'] == dis][['Precaution_1','Precaution_2', 'Precaution_3', 'Precaution_4']]
-  pre = [col for col in pre.values]
+    pre = precaucation[precaucation['Disease'] == dis][['Precaution_1', 'Precaution_2', 'Precaution_3', 'Precaution_4']]
+    pre = [col for col in pre.values]
 
-  med = medication[medication['Disease']==dis]['Medication']
-  med = [med for med in med.values]
+    med = medication[medication['Disease'] == dis]['Medication']
+    med = [med for med in med.values]
 
-  die = diets[diets['Disease']==dis]['Diet']
-  die = [w for w in die.values]
+    die = diets[diets['Disease'] == dis]['Diet']
+    die = [w for w in die.values]
 
-  wrkout = workout[workout['disease'] == dis]['workout']
-  return descr, pre, med, die, wrkout
+    wrkout = workout[workout['disease'] == dis]['workout']
+    return descr, pre, med, die, wrkout
 
-symptoms_dict = {'itching': 0, 'skin_rash': 1, 'nodal_skin_eruptions': 2, 'continuous_sneezing': 3, 'shivering': 4, 'chills': 5, 'joint_pain': 6, 'stomach_pain': 7, 'acidity': 8, 'ulcers_on_tongue': 9, 'muscle_wasting': 10, 'vomiting': 11, 'burning_micturition': 12, 'spotting_ urination': 13, 'fatigue': 14, 'weight_gain': 15, 'anxiety': 16, 'cold_hands_and_feets': 17, 'mood_swings': 18, 'weight_loss': 19, 'restlessness': 20, 'lethargy': 21, 'patches_in_throat': 22, 'irregular_sugar_level': 23, 'cough': 24, 'high_fever': 25, 'sunken_eyes': 26, 'breathlessness': 27, 'sweating': 28, 'dehydration': 29, 'indigestion': 30, 'headache': 31, 'yellowish_skin': 32, 'dark_urine': 33, 'nausea': 34, 'loss_of_appetite': 35, 'pain_behind_the_eyes': 36, 'back_pain': 37, 'constipation': 38, 'abdominal_pain': 39, 'diarrhoea': 40, 'mild_fever': 41, 'yellow_urine': 42, 'yellowing_of_eyes': 43, 'acute_liver_failure': 44, 'fluid_overload': 45, 'swelling_of_stomach': 46, 'swelled_lymph_nodes': 47, 'malaise': 48, 'blurred_and_distorted_vision': 49, 'phlegm': 50, 'throat_irritation': 51, 'redness_of_eyes': 52, 'sinus_pressure': 53, 'runny_nose': 54, 'congestion': 55, 'chest_pain': 56, 'weakness_in_limbs': 57, 'fast_heart_rate': 58, 'pain_during_bowel_movements': 59, 'pain_in_anal_region': 60, 'bloody_stool': 61, 'irritation_in_anus': 62, 'neck_pain': 63, 'dizziness': 64, 'cramps': 65, 'bruising': 66, 'obesity': 67, 'swollen_legs': 68, 'swollen_blood_vessels': 69, 'puffy_face_and_eyes': 70, 'enlarged_thyroid': 71, 'brittle_nails': 72, 'swollen_extremeties': 73, 'excessive_hunger': 74, 'extra_marital_contacts': 75, 'drying_and_tingling_lips': 76, 'slurred_speech': 77, 'knee_pain': 78, 'hip_joint_pain': 79, 'muscle_weakness': 80, 'stiff_neck': 81, 'swelling_joints': 82, 'movement_stiffness': 83, 'spinning_movements': 84, 'loss_of_balance': 85, 'unsteadiness': 86, 'weakness_of_one_body_side': 87, 'loss_of_smell': 88, 'bladder_discomfort': 89, 'foul_smell_of urine': 90, 'continuous_feel_of_urine': 91, 'passage_of_gases': 92, 'internal_itching': 93, 'toxic_look_(typhos)': 94, 'depression': 95, 'irritability': 96, 'muscle_pain': 97, 'altered_sensorium': 98, 'red_spots_over_body': 99, 'belly_pain': 100, 'abnormal_menstruation': 101, 'dischromic _patches': 102, 'watering_from_eyes': 103, 'increased_appetite': 104, 'polyuria': 105, 'family_history': 106, 'mucoid_sputum': 107, 'rusty_sputum': 108, 'lack_of_concentration': 109, 'visual_disturbances': 110, 'receiving_blood_transfusion': 111, 'receiving_unsterile_injections': 112, 'coma': 113, 'stomach_bleeding': 114, 'distention_of_abdomen': 115, 'history_of_alcohol_consumption': 116, 'fluid_overload.1': 117, 'blood_in_sputum': 118, 'prominent_veins_on_calf': 119, 'palpitations': 120, 'painful_walking': 121, 'pus_filled_pimples': 122, 'blackheads': 123, 'scurring': 124, 'skin_peeling': 125, 'silver_like_dusting': 126, 'small_dents_in_nails': 127, 'inflammatory_nails': 128, 'blister': 129, 'red_sore_around_nose': 130, 'yellow_crust_ooze': 131}
-diseases_dict = {15: 'Fungal infection', 4: 'Allergy', 16: 'GERD', 9: 'Chronic cholestasis', 14: 'Drug Reaction', 33: 'Peptic ulcer diseae', 1: 'AIDS', 12: 'Diabetes ', 17: 'Gastroenteritis', 6: 'Bronchial Asthma', 23: 'Hypertension ', 30: 'Migraine', 7: 'Cervical spondylosis', 32: 'Paralysis (brain hemorrhage)', 28: 'Jaundice', 29: 'Malaria', 8: 'Chicken pox', 11: 'Dengue', 37: 'Typhoid', 40: 'hepatitis A', 19: 'Hepatitis B', 20: 'Hepatitis C', 21: 'Hepatitis D', 22: 'Hepatitis E', 3: 'Alcoholic hepatitis', 36: 'Tuberculosis', 10: 'Common Cold', 34: 'Pneumonia', 13: 'Dimorphic hemmorhoids(piles)', 18: 'Heart attack', 39: 'Varicose veins', 26: 'Hypothyroidism', 24: 'Hyperthyroidism', 25: 'Hypoglycemia', 31: 'Osteoarthristis', 5: 'Arthritis', 0: '(vertigo) Paroymsal  Positional Vertigo', 2: 'Acne', 38: 'Urinary tract infection', 35: 'Psoriasis', 27: 'Impetigo'}
 
+symptoms_dict = {
+    'itching': 0, 'skin_rash': 1, 'nodal_skin_eruptions': 2, 'continuous_sneezing': 3, 'shivering': 4, 'chills': 5,
+    'joint_pain': 6, 'stomach_pain': 7, 'acidity': 8, 'ulcers_on_tongue': 9, 'muscle_wasting': 10, 'vomiting': 11,
+    'burning_micturition': 12, 'spotting_ urination': 13, 'fatigue': 14, 'weight_gain': 15, 'anxiety': 16,
+    'cold_hands_and_feets': 17, 'mood_swings': 18, 'weight_loss': 19, 'restlessness': 20, 'lethargy': 21,
+    'patches_in_throat': 22, 'irregular_sugar_level': 23, 'cough': 24, 'high_fever': 25, 'sunken_eyes': 26,
+    'breathlessness': 27, 'sweating': 28, 'dehydration': 29, 'indigestion': 30, 'headache': 31, 'yellowish_skin': 32,
+    'dark_urine': 33, 'nausea': 34, 'loss_of_appetite': 35, 'pain_behind_the_eyes': 36, 'back_pain': 37,
+    'constipation': 38, 'abdominal_pain': 39, 'diarrhoea': 40, 'mild_fever': 41, 'yellow_urine': 42,
+    'yellowing_of_eyes': 43, 'acute_liver_failure': 44, 'fluid_overload': 45, 'swelling_of_stomach': 46,
+    'swelled_lymph_nodes': 47, 'malaise': 48, 'blurred_and_distorted_vision': 49, 'phlegm': 50, 'throat_irritation': 51,
+    'redness_of_eyes': 52, 'sinus_pressure': 53, 'runny_nose': 54, 'congestion': 55, 'chest_pain': 56, 'weakness_in_limbs': 57,
+    'fast_heart_rate': 58, 'pain_during_bowel_movements': 59, 'pain_in_anal_region': 60, 'bloody_stool': 61, 'irritation_in_anus': 62,
+    'neck_pain': 63, 'dizziness': 64, 'cramps': 65, 'bruising': 66, 'obesity': 67, 'swollen_legs': 68, 'swollen_blood_vessels': 69,
+    'puffy_face_and_eyes': 70, 'enlarged_thyroid': 71, 'brittle_nails': 72, 'swollen_extremities': 73, 'excessive_hunger': 74,
+    'extra_marital_contacts': 75, 'drying_and_tingling_lips': 76, 'slurred_speech': 77, 'knee_pain': 78,
+    'hip_joint_pain': 79, 'muscle_weakness': 80, 'stiff_neck': 81, 'swelling_joints': 82, 'movement_stiffness': 83,
+    'spinning_movements': 84, 'loss_of_balance': 85, 'unsteadiness': 86, 'weakness_of_one_body_side': 87,
+    'loss_of_smell': 88, 'bladder_discomfort': 89, 'foul_smell_of urine': 90, 'continuous_feel_of_urine': 91,
+    'passage_of_gases': 92, 'internal_itching': 93, 'toxic_look_(typhos)': 94, 'depression': 95, 'irritability': 96,
+    'muscle_pain': 97, 'altered_sensorium': 98, 'red_spots_over_body': 99, 'belly_pain': 100, 'abnormal_menstruation': 101,
+    'dischromic _patches': 102, 'watering_from_eyes': 103, 'increased_appetite': 104, 'polyuria': 105, 'family_history': 106,
+    'mucoid_sputum': 107, 'rusty_sputum': 108, 'lack_of_concentration': 109, 'visual_disturbances': 110,
+    'receiving_blood_transfusion': 111, 'receiving_unsterile_injections': 112, 'coma': 113, 'stomach_bleeding': 114,
+    'distention_of_abdomen': 115, 'history_of_alcohol_consumption': 116, 'fluid_overload': 117, 'blood_in_sputum': 118,
+    'prominent_veins_on_calf': 119, 'palpitations': 120, 'painful_walking': 121, 'pus_filled_pimples': 122,
+    'blackheads': 123, 'scurring': 124, 'skin_peeling': 125, 'silver_like_dusting': 126, 'small_dents_in_nails': 127,
+    'inflammatory_nails': 128, 'blister': 129, 'red_sore_around_nose': 130, 'yellow_crust_ooze': 131
+}
+diseases_dict = {
+    15: 'Fungal infection', 4: 'Allergy', 16: 'GERD', 9: 'Chronic cholestasis', 14: 'Drug Reaction', 33: 'Peptic ulcer disease',
+    1: 'AIDS', 12: 'Diabetes', 17: 'Gastroenteritis', 6: 'Bronchial Asthma', 5: 'Hypertension', 20: 'Migraine',
+    41: 'Cervical spondylosis', 21: 'Paralysis (brain hemorrhage)', 19: 'Jaundice', 28: 'Malaria', 29: 'Chicken pox',
+    44: 'Dengue', 37: 'Typhoid', 45: 'hepatitis A', 25: 'Hepatitis B', 26: 'Hepatitis C', 27: 'Hepatitis D',
+    24: 'Hepatitis E', 40: 'Alcoholic hepatitis', 10: 'Tuberculosis', 39: 'Common Cold', 35: 'Pneumonia',
+    18: 'Dimorphic hemmorhoids(piles)', 42: 'Heart attack', 34: 'Varicose veins', 43: 'Hypothyroidism', 38: 'Hyperthyroidism',
+    31: 'Hypoglycemia', 32: 'Osteoarthristis', 30: 'Arthritis', 22: '(vertigo) Paroymsal  Positional Vertigo', 23: 'Acne',
+    2: 'Peptic ulcer diseae', 3: 'Dimorphic hemmorhoids(piles)', 7: 'Hypertension ', 8: 'Migraine', 11: 'Cervical spondylosis',
+    13: 'Paralysis (brain hemorrhage)', 36: 'Jaundice', 46: 'Chicken pox', 47: 'Dengue', 48: 'Typhoid', 49: 'hepatitis A',
+    50: 'Hepatitis B', 51: 'Hepatitis C', 52: 'Hepatitis D', 53: 'Hepatitis E', 54: 'Alcoholic hepatitis', 55: 'Tuberculosis',
+    56: 'Common Cold', 57: 'Pneumonia', 58: 'Dimorphic hemmorhoids(piles)', 59: 'Heart attack', 60: 'Varicose veins',
+    61: 'Hypothyroidism', 62: 'Hyperthyroidism', 63: 'Hypoglycemia', 64: 'Osteoarthristis', 65: 'Arthritis',
+    66: '(vertigo) Paroymsal  Positional Vertigo', 67: 'Acne'
+}
 
 # model prediction Function
-
 def get_predicted_values(patient_symptoms):
-  inp_vector = np.zeros(len(symptoms_dict))
+    inp_vector = np.zeros(len(symptoms_dict))
 
-  for i in patient_symptoms:
-    inp_vector[symptoms_dict[i]] = 1
-  return diseases_dict[svc.predict([inp_vector])[0]]
-# ===============================================================================================
+    for i in patient_symptoms:
+        inp_vector[symptoms_dict[i]] = 1
+    return diseases_dict[svc.predict([inp_vector])[0]]
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
@@ -91,9 +131,8 @@ def predict():
         predicted_disease = get_predicted_values(user_symptoms)
 
         descr, pre, med, die, wrkout = helper(predicted_disease)
-        
 
-        return render_template('index.html',user_symptoms=symptoms, predicted_disease=predicted_disease, dis_des=descr, dis_pre=pre, dis_med=med,dis_diet=die, dis_wrkout=wrkout)
+        return render_template('index.html', user_symptoms=symptoms, predicted_disease=predicted_disease, dis_des=descr, dis_pre=pre, dis_med=med, dis_diet=die, dis_wrkout=wrkout)
 
 
 if __name__ == '__main__':
